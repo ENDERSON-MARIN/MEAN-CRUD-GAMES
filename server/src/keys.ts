da3d1fbb7b9c9099217d96f1d4937cbfd254b6d2
ICOVZLA <marinenderson1@gmail.com>
2019-08-22 09:33:41 -0500

    agregando Dockerfiles and .dockerignore

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -3,8 +3,8 @@ export default {
     database: {
         host: 'localhost',
         user: 'root',
-        password: '$erickson$',
-        database: 'ng_games'
+        password: '',
+        database: 'db_games'
     }
 
 }
